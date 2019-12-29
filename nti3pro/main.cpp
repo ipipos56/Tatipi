fa9dedf6a2646cb0b64b6a4a1f06cac4d9836fc1
ipipos56 <ipipos56@bk.ru>
2019-12-29 12:03:04 +0300

    nti3pro Update
    Array Set NEW

--- nti3pro/main.cpp
+++ nti3pro/main.cpp
@@ -43,11 +43,18 @@ int staXY(int _xy, bool pal,bool pos)
 
 int main()
 {
+    freopen("input.txt","r",stdin);
     cin>>h;
     h = h/250+1;
-    string wall[h][h]={"-1"};
+    string wall[h][h];
+    for(int i = 0;i<h;i++)
+        for(int j = 0; j<h;j++)
+            wall[i][j] = "-1";
     h-=1;
-    int lab[h*h][h*h]={0};
+    bool lab[h*h][h*h];
+    for(int i = 0;i<h*h;i++)
+        for(int j = 0; j<h*h;j++)
+            lab[i][j] = false;
     int n,k;
     cin>>n>>k;
     int alp;
@@ -62,11 +69,69 @@ int main()
     fnX = staXY(fnX,false,true);
     fnY = staXY(fnY,true,true);
     stY = staXY(stY,true,true);
-    for(int i = 0;i<n;i++)
+    for(int j = 0;j<h-1;j++)
+    {
+        for(int i = j*h;i<j*h + h-1;i++)
+        {
+            lab[i][i+1] = true;
+            lab[i+1][i] = true;
+            lab[i+h][i] = true;
+            lab[i][i+h] = true;
+        }
+    }
+    for(int i = h*h-h;i<h*h-1;i++)
+    {
+        lab[i][i+1] = true;
+        lab[i+1][i] = true;
+    }
+
+    for(int i = 0;i<h*h;i++)
+    {
+        for(int j = 0; j<h*h;j++)
+            cout<<lab[i][j]<<" ";
+        cout<<endl;
+    }
+    cout<<endl;
+
+    //cout<<stX<<" "<<stY<<" "<<fnX<<" "<<fnY<<endl;
+    for(int i = 0;i<1;i++)
     {
         int wasX,wafX,wasY,wafY;
-        string wallCol;
-        cin>>wasX>>wasY>>wafX>>wafY>>wallCol;
-
+        string colWa;
+        cin>>wasX>>wasY>>wafX>>wafY>>colWa;
+        wasX = staXY(wasX,false,false);
+        wafX = staXY(wafX,false,false);
+        wafY = staXY(wafY,true,false);
+        wasY = staXY(wasY,true,false);
+        cout<<wasX<<" "<<wasY<<" "<<wafX<<" "<<wafY<<endl;
+        if(wasX == wafX)
+        {
+            int ma = min(wasY,wafY);
+            int maa = max(wasY,wafY);
+            for(int j = ma;j<maa;j++)
+            {
+                wall[j][wasX] = colWa;
+                lab[wasX + j * h][(h+1) * j] = false;
+                lab[(h+1) * j][wasX + j * h] = false;
+            }
+        }
+        else if(wasY == wafY)
+        {
+            int ma = min(wasX,wafX);
+            int maa = max(wasX,wafX);
+            for(int j = ma;j<maa;j++)
+            {
+                wall[wasY][j] = colWa;
+                lab[h * j+wasX][h * j+wasX+1] = false;
+                lab[h * j+wasX+1][h * j+wasX] = false;
+            }
+        }
+        //cout<<wasX<<" "<<wasY<<" "<<wafX<<" "<<wafY<<endl;
+    }
+    for(int i = 0;i<h*h;i++)
+    {
+        for(int j = 0; j<h*h;j++)
+            cout<<lab[i][j]<<" ";
+        cout<<endl;
     }
 }
