63d6a5e78e896db6fa71147469b552752ab11ccf
ipipos56 <ipipos56@bk.ru>
2020-01-06 16:30:01 +0300

    ntiLast names

--- ntiLast/main.cpp
+++ ntiLast/main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-map<int,string> ma;
+map<int,string> ma; // Color map
 map<int,string>:: iterator mit;
 
 int couColor = 0;
@@ -18,8 +18,8 @@ int main()
     cin>>ds;
     int pX,pY;
     float beta;
-    cin>>pX>>pY>>beta;
-    int wall[n][5];
+    cin>>pX>>pY>>beta; // X and Y of robot
+    int wall[n][5]; // Walls array
     for(int i = 0;i<n;i++)
         for(int j = 0; j<5;j++)
             wall[i][j] = -1;
@@ -37,25 +37,25 @@ int main()
 
 
         int ins = 0;
-        for(mit = ma.begin(); mit!=ma.end();mit++)
+        for(mit = ma.begin(); mit!=ma.end();mit++) // Find old color
             if(mit->second == colWa)
                 ins = mit->first;
-        if(ins == 0)
+        if(ins == 0) // new Color set
         {
             ma[couColor] = colWa;
             ins = couColor;
             couColor++;
         }
-        wall[i][4] = ins;
+        wall[i][4] = ins; // Color of wall
 
         if(i != 0)
         {
             for(int j = 0;j<i;j++)
             {
                 if(wall[i][0] == wall[j][0] && wall[i][1] == wall[j][1])
-                    cout<<wall[i][0]<<" "<<wall[i][1]<<endl;
+                    cout<<wall[i][0]<<" "<<wall[i][1]<<" StartCol"<<endl;
                 if(wall[i][2] == wall[j][2] && wall[i][3] == wall[j][3])
-                    cout<<wall[i][2]<<" "<<wall[i][3]<<endl;
+                    cout<<wall[i][2]<<" "<<wall[i][3]<<" FinishCol"<<endl;
             }
         }
     }
