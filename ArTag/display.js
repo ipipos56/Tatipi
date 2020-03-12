33721760c902995b031e1abb2f9faa5a5692e1b3
JoshLeonid <59415766+JoshLeonid@users.noreply.github.com>
2020-03-12 15:51:22 +0300

    Create display.js

---
+++ ArTag/display.js
@@ -0,0 +1,11 @@
+var __interpretation_started_timestamp__;
+var pi = 3.141592653589793;
+var disp = brick.display();
+var main = function()
+{
+	disp.setPainterColor("#000000")
+	disp.setPainterWidth(3)
+	disp.drawLine(0,0,50,100);
+	disp.redraw();
+	script.wait(1000000);
+}
