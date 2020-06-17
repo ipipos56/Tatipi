ea96955fae3ad708c6d6d5c6a1326aadb0e2615f
No <No@no.no>
2020-06-18 02:07:57 +0500

    prefinal

--- RRO2020TatipiReborn/ivanV2Old.js
+++ RRO2020TatipiReborn/ivanV2Old.js
@@ -11,18 +11,9 @@ a2=brick.sensor(A2).read;
 a3=brick.sensor(A3).read;
 a4=brick.sensor(A4).read;
 a5middle=brick.sensor(A5).read;
-a6back=brick.sensor(A6).read;
 inverted_section=false;
 delay=script.wait
 abs=Math.abs
-max_cross_value=0
-max_cross_x=1
-max_cross_y=1
-
-back_free=true
-left_free=false
-right_free=false
-forward_free=true
 
 
 ml(-100)
@@ -36,8 +27,9 @@ pathminus=0
 el.reset()
 er.reset()
 
+
 x_start=3
-y_start=1
+y_start=0
 
 
 
@@ -88,19 +80,6 @@ for(var i=0;i<11;i++){
 }
 var main = function(){
 	go_to_coordinates()
-	
-	ml(100)
-	mr(100)
-	delay(300)
-	ml(0)
-	mr(0)
-	delay(5000)
-	///BASESESESE
-	ml(-100)
-	mr(-100)
-	delay(300)
-	ml(0)
-	mr(0)
 	for(var i=x_base-4;i<=x_base+4;i+=2){
 		for(var j=y_base-4;j<=y_base+4;j+=2){
 			if(i>=0 && j>=0 && i<30 && j<30 && mapVisit[i][j]!=1){
@@ -111,33 +90,9 @@ var main = function(){
 		}
 	}
 	path_check();
-	calculate_path();
 	ml(-100)
 	mr(-100)
 	delay(400)
-	
-	x_fin=max_cross_x
-	y_fin=max_cross_y
-	go_to_coordinates()
-	ml(100)
-	mr(100)
-	delay(300)
-	ml(0)
-	mr(0)
-	////STATION
-	ml(-100)
-	mr(-100)
-	delay(300)
-	ml(0)
-	mr(0)
-	x_fin=15
-	y_fin=15
-	go_to_coordinates()
-	ml(100)
-	mr(100)
-	delay(300)
-	ml(0)
-	mr(0)
 }
 
 function go_to_coordinates(){
@@ -147,142 +102,96 @@ function go_to_coordinates(){
 		return
 	}
 	
-	slew=false
 	while(!(x_pos==x_fin&&y_pos==y_fin)){
-		print(x_pos+" "+y_pos)
-		print(x_fin+" "+y_fin)
-		//if(!slew){
+			print(x_pos+" "+y_pos)
+			print(x_fin+" "+y_fin)
 			path_check();
-		//}
-		calculate_path()
-		
-		//printMapPaint(mapCross)
-		//delay(1000)
-		if(terr){
-			ml(-100)
-			mr(-100)
-			delay(400)
-			return
-		}
-		
-		movedir=0;
-		if(mapPaint[x_pos+1][y_pos]<mapPaint[x_pos][y_pos]){
-			movedir=1;
-		}else{
-			if(mapPaint[x_pos-1][y_pos]<mapPaint[x_pos][y_pos]){
-				movedir=3;
+			calculate_path()
+			if(terr){
+				ml(-100)
+				mr(-100)
+				delay(400)
+				return
+			}
+			
+			movedir=0;
+			if(mapPaint[x_pos+1][y_pos]<mapPaint[x_pos][y_pos]){
+				movedir=1;
 			}else{
-				if(mapPaint[x_pos][y_pos+1]<mapPaint[x_pos][y_pos]){
-					movedir=2;
+				if(mapPaint[x_pos-1][y_pos]<mapPaint[x_pos][y_pos]){
+					movedir=3;
 				}else{
-					if(mapPaint[x_pos][y_pos-1]<mapPaint[x_pos][y_pos]){
-						movedir=0;
+					if(mapPaint[x_pos][y_pos+1]<mapPaint[x_pos][y_pos]){
+						movedir=2;
 					}else{
-						//break;
+						if(mapPaint[x_pos][y_pos-1]<mapPaint[x_pos][y_pos]){
+							movedir=0;
+						}else{
+							//break;
+						}
 					}
 				}
 			}
-		}
-		
-		
-		if(rot==0 && movedir==0){
 			
-		}
-		if(rot==0 && movedir==1){
-			slew=false
-			turn_right();
-		}
-		if(rot==0 && movedir==2){
-			rotate();
-			slew=true
-		}
-		if(rot==0 && movedir==3){
-			slew=false
-			turn_left();
-		}
-		
-		
-		if(rot==1 && movedir==0){
-			slew=false
-			turn_left();
-		}
-		if(rot==1 && movedir==1){
 			
-		}
-		if(rot==1 && movedir==2){
-			slew=false
-			turn_right();
-		}
-		if(rot==1 && movedir==3){
-			rotate();
-			slew=true
-		}
-		
-		
-		if(rot==2 && movedir==0){
-			rotate();
-			slew=true
-		}
-		if(rot==2 && movedir==1){
-			slew=false
-			turn_left();
-		}
-		if(rot==2 && movedir==2){
 			
-		}
-		if(rot==2 && movedir==3){
-			slew=false
-			turn_right();
-		}
-		
-		
-		if(rot==3 && movedir==0){
-			slew=false
-			turn_right();
-		}
-		if(rot==3 && movedir==1){
-			rotate();
-			slew=true
-		}
-		if(rot==3 && movedir==2){
-			slew=false
-			turn_left();
-		}
-		if(rot==3 && movedir==3){
-			
-		}
-		if(slew){
-			print(left_free)
-			print(right_free)
-			if(!left_free&&!right_free){
-				print("ygtfaughjfvutyfshgb")
-				ml(100)
-				mr(100)
-				delay(600)
-				ml(0)
-				mr(0)
+			if(rot==0 && movedir==0){
+				
 			}
-			forward();
-		}else{
+			if(rot==0 && movedir==1){
+				turn_right();
+			}
+			if(rot==0 && movedir==2){
+				rotate();
+			}
+			if(rot==0 && movedir==3){
+				turn_left();
+			}
+			
+			
+			if(rot==1 && movedir==0){
+				turn_left();
+			}
+			if(rot==1 && movedir==1){
+				
+			}
+			if(rot==1 && movedir==2){
+				turn_right();
+			}
+			if(rot==1 && movedir==3){
+				rotate();
+			}
+			
+			
+			if(rot==2 && movedir==0){
+				rotate();
+			}
+			if(rot==2 && movedir==1){
+				turn_left();
+			}
+			if(rot==2 && movedir==2){
+				
+			}
+			if(rot==2 && movedir==3){
+				turn_right();
+			}
+			
+			
+			if(rot==3 && movedir==0){
+				turn_right();
+			}
+			if(rot==3 && movedir==1){
+				rotate();
+			}
+			if(rot==3 && movedir==2){
+				turn_left();
+			}
+			if(rot==3 && movedir==3){
+				
+			}
+			
 			forward();
 		}
-	
-		
-		mapVisit[x_pos][y_pos]=1
-		mapVisit[x_pos][y_pos-1]=1
-		mapVisit[x_pos][y_pos+1]=1
-		mapVisit[x_pos-1][y_pos]=1
-		mapVisit[x_pos+1][y_pos]=1
-		mapVisit[x_pos-1][y_pos-1]=1
-		mapVisit[x_pos+1][y_pos+1]=1
-		mapVisit[x_pos-1][y_pos+1]=1
-		mapVisit[x_pos+1][y_pos-1]=1
-		
-		//printMapPaint(map)
-	}
-	ml(0)
-	mr(0)
-	
 }
 
 
@@ -290,8 +199,7 @@ function printMapPaint(msn){
 	for(var i=0;i<msn.length;i++){
 		var st="";
 		for(var j=0;j<msn[i].length;j++){
-			if(j==x_fin&&i==y_fin)st+="  ?";
-				else if(j==x_pos&&i==y_pos)st+="  X";
+			if(j==x_pos&&i==y_pos)st+="  X";
 				else if(msn[j][i]>=100)st+="   ";
 				else if(msn[j][i]>=10)st+=" "+msn[j][i];
 				else if(msn[j][i]>=0)st+="  "+msn[j][i];
@@ -301,8 +209,6 @@ function printMapPaint(msn){
 }
 
 function calculate_path(){
-	ml(0)
-	mr(0)
 	//print(xfin+" "+yfin)
 	for(var i=0;i<30;i++){
 		mapPaint[i]=new Array(30);
@@ -339,24 +245,14 @@ function calculate_path(){
 	}else{
 		terr=false;
 	}
+	printMapPaint(map)
 }
 
 function path_check(){
-	back_free=true
+	
 	left_free=false
 	right_free=false
 	forward_free=true
-	if(inverted_section){
-		if(a6back()>50&&back_free){
-			back_free=false
-		}
-	}else{
-		if(a6back()<10&&back_free){
-			back_free=false
-		}
-	}
-	
-	
 	fullmoved=false
 	deg = (el.read()+er.read())/2 + (700/(pi*56))*110;
 	while(true){
@@ -406,9 +302,6 @@ function path_check(){
 	}
 	if(forward_free){
 		print("FORWARD_FREE")
-	}
-	if(back_free){
-		print("BACK_FREE")
 	}
 	if(fullmoved){
 		pathminus=(700/(pi*56))*30
@@ -489,89 +382,14 @@ function path_check(){
 		}
 	}
 	
-	cross_k=0
-	
-	if(left_free)cross_k+=1
-	if(right_free)cross_k+=1
-	if(back_free)cross_k+=1
-	if(forward_free)cross_k+=1
-		
-	if(cross_k>=3 && abs(x_pos-x_base)<=2*2 && abs(y_pos-y_base)<=2*2){
-		crossValue=1
-		temp_pos_x=x_pos-1
-		temp_pos_y=y_pos
-		if(!(abs(temp_pos_x-x_base)<=2*2 && abs(temp_pos_y-y_base)<=2*2)){
-			if(temp_pos_x<=maxx&&temp_pos_x>=minx && temp_pos_y<=maxy && temp_pos_y>=miny){
-				crossValue+=1;
-			}
-		}
-		temp_pos_x=x_pos+1
-		temp_pos_y=y_pos
-		if(!(abs(temp_pos_x-x_base)<=2*2 && abs(temp_pos_y-y_base)<=2*2)){
-			if(temp_pos_x<=maxx&&temp_pos_x>=minx && temp_pos_y<=maxy && temp_pos_y>=miny){
-				crossValue+=1;
-			}
-		}
-		temp_pos_x=x_pos
-		temp_pos_y=y_pos-1
-		if(!(abs(temp_pos_x-x_base)<=2*2 && abs(temp_pos_y-y_base)<=2*2)){
-			if(temp_pos_x<=maxx&&temp_pos_x>=minx && temp_pos_y<=maxy && temp_pos_y>=miny){
-				crossValue+=1;
-			}
-		}
-		temp_pos_x=x_pos
-		temp_pos_y=y_pos+1
-		if(!(abs(temp_pos_x-x_base)<=2*2 && abs(temp_pos_y-y_base)<=2*2)){
-			if(temp_pos_x<=maxx&&temp_pos_x>=minx && temp_pos_y<=maxy && temp_pos_y>=miny){
-				crossValue+=1;
-			}
-		}
-		temp_pos_x=x_pos-1
-		temp_pos_y=y_pos-1
-		if(!(abs(temp_pos_x-x_base)<=2*2 && abs(temp_pos_y-y_base)<=2*2)){
-			if(temp_pos_x<=maxx&&temp_pos_x>=minx && temp_pos_y<=maxy && temp_pos_y>=miny){
-				crossValue+=1;
-			}
-		}
-		temp_pos_x=x_pos-1
-		temp_pos_y=y_pos+1
-		if(!(abs(temp_pos_x-x_base)<=2*2 && abs(temp_pos_y-y_base)<=2*2)){
-			if(temp_pos_x<=maxx&&temp_pos_x>=minx && temp_pos_y<=maxy && temp_pos_y>=miny){
-				crossValue+=1;
-			}
-		}
-		temp_pos_x=x_pos+1
-		temp_pos_y=y_pos-1
-		if(!(abs(temp_pos_x-x_base)<=2*2 && abs(temp_pos_y-y_base)<=2*2)){
-			if(temp_pos_x<=maxx&&temp_pos_x>=minx && temp_pos_y<=maxy && temp_pos_y>=miny){
-				crossValue+=1;
-			}
-		}
-		temp_pos_x=x_pos+1
-		temp_pos_y=y_pos+1
-		if(!(abs(temp_pos_x-x_base)<=2*2 && abs(temp_pos_y-y_base)<=2*2)){
-			if(temp_pos_x<=maxx&&temp_pos_x>=minx && temp_pos_y<=maxy && temp_pos_y>=miny){
-				crossValue+=1;
-			}
-		}
-		if(crossValue>=max_cross_value){
-			max_cross_x=x_pos
-			max_cross_y=y_pos
-			max_cross_value=crossValue
-		}
-		mapCross[x_pos][y_pos]=crossValue
-	}
-
-	
-	
-	//ml(0)
-	//mr(0)
-	//delay(2000)
+	ml(0)
+	mr(0)
+	delay(2000)
 }
 
 function forward(){
 	
-	deg = (el.read()+er.read())/2 + (700/(pi*56))*100-pathminus;
+	deg = (el.read()+er.read())/2 + (700/(pi*56))*90-pathminus;
 	while((el.read()+er.read())/2 < deg){
 		if(a3()>50&&a4()>50){
 			inverted_section=true
@@ -621,15 +439,9 @@ function turn_left(){
 	rot = cuboid(rot);
 }
 function rotate(){
-	if(left_free){
-		ml(-100)
-		mr(100)
-		delay(1000)
-	}else{
-		ml(100)
-		mr(-100)
-		delay(1000)
-	}
+	ml(-100)
+	mr(100)
+	delay(1000)
 	rot-=2; 
 	rot = cuboid(rot);
 }
